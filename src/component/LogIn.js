622cdf5334ceace150e896f1bdab86390a68d753
kumol <kumol@yotech.ltd>
2022-11-04 18:50:16 +0600

    sef

--- src/component/LogIn.js
+++ src/component/LogIn.js
@@ -107,6 +107,12 @@ const LogIn = () => {
             </div>
 
         </div>
+
+        <div>
+            <div className={classes.typewriter}>
+                <h1>The cat and the hat.</h1>
+            </div>
+        </div>
     </div >)
 }
 
